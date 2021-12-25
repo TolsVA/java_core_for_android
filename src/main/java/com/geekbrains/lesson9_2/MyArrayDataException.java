f6aab1b1a9d5ea9597f48214fa4176854570f979
TolsVA <himtol@yandex.ru>
2021-12-26 05:46:29 +0700

    Revert "add lesson10"
    This reverts commit ac9b34edd34ec9d77868bf52f7e1be8ebbceabea.

--- src/main/java/com/geekbrains/lesson9_2/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.lesson9_2;
-
-public class MyArrayDataException extends NumberFormatException {
-    public MyArrayDataException(String message) {
-        super("MyArrayDataException - " + message);
-    }
-}
--- src/main/java/com/geekbrains/lesson9_2/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package com.geekbrains.lesson9_2;
-
-public class MyArraySizeException extends ArrayIndexOutOfBoundsException{
-    public MyArraySizeException(String message) {
-        super("MyArraySizeException - " + message);
-    }
-}
